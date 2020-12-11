e589fdc495a571efd74e58a81bda16887a3060b6
elvimiraka <elvi7miraka@gmail.com>
2020-12-11 09:47:24 +0100

    Updates

---
+++ precache-manifest.d0522d8838dd20bc57310b91498a18b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2076597e2c811d3cbe654a96896d68",
+    "url": "/lexoj.al/index.html"
+  },
+  {
+    "revision": "cedd0cae06ca00617220",
+    "url": "/lexoj.al/static/css/main.06910e0f.chunk.css"
+  },
+  {
+    "revision": "e81d5713a0a9c248d617",
+    "url": "/lexoj.al/static/js/2.f2941d34.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/lexoj.al/static/js/2.f2941d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cedd0cae06ca00617220",
+    "url": "/lexoj.al/static/js/main.e6792da0.chunk.js"
+  },
+  {
+    "revision": "76d73fb6d14801676403",
+    "url": "/lexoj.al/static/js/runtime-main.9d04407a.js"
+  },
+  {
+    "revision": "daff393ef25c1086d4af733a310d874a",
+    "url": "/lexoj.al/static/media/background.daff393e.jpg"
+  },
+  {
+    "revision": "e2e08f80b4159a0fc56c9947e5a147bf",
+    "url": "/lexoj.al/static/media/baner.e2e08f80.png"
+  },
+  {
+    "revision": "5052d102c5af81af3d7f5bf3fbc69202",
+    "url": "/lexoj.al/static/media/lexojlogo.5052d102.png"
+  }
+]);
