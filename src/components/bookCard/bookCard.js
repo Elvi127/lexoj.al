3bec344c7fdeb1e428b78834095707b5b2342a2d
elvimiraka <elvi7miraka@gmail.com>
2020-12-24 21:59:47 +0100

    Total sales icon change

--- src/components/bookCard/bookCard.js
+++ src/components/bookCard/bookCard.js
@@ -13,7 +13,7 @@ const BookCard = props => {
                     <div className='card-subdetails'>
                         <p className='card-author'>{props.author}</p>
                         <div className='card-views'>
-                            <p className='card-icon'><ion-icon name="eye-sharp"></ion-icon></p>
+                            <p className='card-icon'><ion-icon name="cart-sharp"></ion-icon></p>
                             <p>{props.views}</p>
                         </div>
                     </div>
