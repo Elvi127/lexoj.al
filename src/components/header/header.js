dd0944fb8ed3ebd6689d118e0d7ababb3adf5420
elvimiraka <elvi7miraka@gmail.com>
2020-12-28 15:25:36 +0100

    Profile Icon Header added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,26 +2,36 @@ import React from 'react';
 import Logo from './lexojlogo.png';
 import './header.css';
 import {Link} from 'react-router-dom';
+import ProfileIcon from '../profileIcon/profileIcon';
 
 
 const Header = props =>{
+    const isLoged = true;
     return(
         <div className="header-bar">
             <Link to="/">
-                <img alt="logo" src={Logo} />
+                <img className="logo" alt="logo" src={Logo} />
             </Link>
             <div className="search-bar">
                 <input type='text' />
                 <ion-icon name="search-outline"></ion-icon>
             </div>
-            <div className="log-bar">
-                <Link to="/hyr" style={{ textDecoration: 'none' }}>
-                    <div>Hyr</div>
-                </Link>
-                <Link to="/regjistrohu" style={{ textDecoration: 'none' }}>
-                    <div>Regjistrohu</div>
-                </Link>
-            </div>
+            {
+                isLoged?
+                <div className="log-bar">
+                    <Link to="/llogaria" style={{ textDecoration: 'none' }}>
+                        <ProfileIcon />
+                    </Link>
+                </div>
+                :<div className="log-bar">
+                    <Link to="/hyr" style={{ textDecoration: 'none' }}>
+                        <div>Hyr</div>
+                    </Link>
+                    <Link to="/regjistrohu" style={{ textDecoration: 'none' }}>
+                        <div>Regjistrohu</div>
+                    </Link>
+                </div>
+            } 
         </div>
     )
 }
---
+++ src/components/profileIcon/profileIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './profileIcon.css'
+
+const ProfileIcon = props => {
+    return(
+        <img className="profileImgHeader" alt="profileIcon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLCbQsmw9KpoBCi4Yabq8oau1Ksx3U8rmJBA&usqp=CAU" />
+    )
+}
+
+export default ProfileIcon;
