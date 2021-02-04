aef2c603a9a8c93c291824342f1fee23a9d7b5cf
elvimiraka <elvi7miraka@gmail.com>
2021-02-04 23:52:31 +0100

    Updates

--- precache-manifest.61a00bcc7bc0f683750ee2c7a315d446.js
+++ precache-manifest.61a00bcc7bc0f683750ee2c7a315d446.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "000d012390f1286a109ca4ed2a8dd23c",
+    "revision": "6702e2d6b953dd8b001c9a0d73fdd6a2",
     "url": "/lexoj.al/index.html"
   },
   {
-    "revision": "9a8e8ab1209d33a4c110",
-    "url": "/lexoj.al/static/css/main.bcc7f459.chunk.css"
+    "revision": "8f5948d693ededd90691",
+    "url": "/lexoj.al/static/css/main.c4cdee4e.chunk.css"
   },
   {
-    "revision": "2ffe21989a29a1f480e9",
-    "url": "/lexoj.al/static/js/2.f389fa8a.chunk.js"
+    "revision": "91b6bc128ac804dace88",
+    "url": "/lexoj.al/static/js/2.53352647.chunk.js"
   },
   {
     "revision": "9b318b6fb13190fe82c0677e9264b3c7",
-    "url": "/lexoj.al/static/js/2.f389fa8a.chunk.js.LICENSE.txt"
+    "url": "/lexoj.al/static/js/2.53352647.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a8e8ab1209d33a4c110",
-    "url": "/lexoj.al/static/js/main.caaec510.chunk.js"
+    "revision": "8f5948d693ededd90691",
+    "url": "/lexoj.al/static/js/main.e086604e.chunk.js"
   },
   {
     "revision": "76d73fb6d14801676403",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lexoj.al/precache-manifest.c0b01b6c203610f28a161f2e23aeafd0.js"
+  "/lexoj.al/precache-manifest.61a00bcc7bc0f683750ee2c7a315d446.js"
 );
 
 self.addEventListener('message', (event) => {
