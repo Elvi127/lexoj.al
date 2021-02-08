90ca907503666d4db88ca7d23690db99193d4767
elvimiraka <elvi7miraka@gmail.com>
2021-02-08 23:43:32 +0100

    Login API connected

--- src/pages/signIn/signIn.js
+++ src/pages/signIn/signIn.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import * as userActions from '../../store/actions/user';
+import * as notificationActions from '../../store/actions/notification';
 import Logo from '../../components/header/lexojlogo3.png';
+import URL from '../../components/url';
 import './sign.css';
 
 const SignIn = props => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [userDetails, setDetails] = useState({});
+
+    const onFormValues = (key, value) => {
+        setDetails({...userDetails, [key]: value});
+    }
+    const signInUser = () => {
+        fetch(`${URL}/signin`, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email: userDetails.email,
+                password: userDetails.password,
+            })
+        }).then(response => response.json()).then(data =>{
+            console.log(data)
+            if(data.status === 'success'){
+                dispatch(userActions.loginUser(data.token))
+                dispatch(notificationActions.displayNotification({message: `Ju u kycet me sukses`, priority: 'white'}))
+                history.push('/');
+            }
+        });
+    }
     return(
         <div className="sign-screen">
             <div className="logo-side">
@@ -11,9 +40,9 @@ const SignIn = props => {
             <div className="sign-side">
                 <div className="sign-card">
                     <p>Hyr</p>
-                    <input type="email" placeholder="email" />
-                    <input type="password" placeholder="fjalekalimi"/>
-                    <button>Hyr</button>
+                    <input onChange={(e)=>onFormValues('email', e.target.value)} type="email" placeholder="email" />
+                    <input onChange={(e)=>onFormValues('password', e.target.value)} type="password" placeholder="fjalekalimi"/>
+                    <button onClick={signInUser}>Hyr</button>
                 </div>
             </div>
         </div>
