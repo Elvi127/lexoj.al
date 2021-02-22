a388edc89f35a4e101081f6a2d7b8ee599402cca
elvimiraka <elvi7miraka@gmail.com>
2021-02-22 19:53:49 +0100

    Search Component Api Connected

--- src/components/searchComponent/searchComponent.js
+++ src/components/searchComponent/searchComponent.js
@@ -1,12 +1,21 @@
 import React, {useState} from 'react';
-import {useSelector} from 'react-redux';
 import {Link} from 'react-router-dom';
 import './searchComponent.css';
+import URL from '../url';
 
 const SearchComponent = props => {
     const [searchedTitle, setSearchedTitle] = useState("");
-    const books = useSelector(state => state.books.books);
+    const [books, setBooks] = useState([]);
     let searchedBooks = [];
+    if(searchedTitle.length === 3 && books.length < 1){
+        fetch(`${URL}/title?title=${searchedTitle}`, {
+            method: 'get',
+            headers: {'Content-Type': 'application/json'},
+        }).then(response => response.json()).then(data =>{
+            console.log(data)
+           setBooks(data.data.searchedBooks);
+        });
+    }
     if(searchedTitle.length > 2){
         searchedBooks = books.filter(book => {
             return book.title.toLowerCase().includes(searchedTitle.toLowerCase());
@@ -22,17 +31,20 @@ const SearchComponent = props => {
        }
     
     }
+    const deleteBooks = () => {
+        if(searchedTitle.length < 3) setBooks([]);
+    }
 
     return(
         <div onClick={(e) => onClose(e)} className="searchComponent">
             <div className="searchContainer">
-                <input autoComplete="off" autoFocus={true} name="input" onChange={(e)=> setSearchedTitle(e.target.value)} className="searchTitlesInput" type="text" />
+                <input autoComplete="off" autoFocus={true} name="input" onChange={(e)=> {setSearchedTitle(e.target.value); deleteBooks()}} className="searchTitlesInput" type="text" />
                 <div name="book" className="booksResultsContainer">
                     {
                         searchedBooks.slice(0,3).map(book => {
-                            return <Link to={`/book/${book.title}`} style={{width: '100%', color: 'black', textDecoration: 'none' }}>
+                            return <Link to={`/book/${book._id}`} style={{width: '100%', color: 'black', textDecoration: 'none' }}>
                                 <div className="bookResultContainer">
-                                    <img className="bookResultImage" alt="bookImg" src={book.img} />
+                                    <img className="bookResultImage" alt="bookImg" src={book.imgcover} />
                                     <p className="bookResultTitle">{book.title}</p>
                                     <p className="bookResultAuthor">{book.author}</p>
                                 </div>
